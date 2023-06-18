9c76de61c8893eb6e279240792b2a4005614f43e
Ajay <ajay@Ajays-MacBook-Air.local>
2023-06-18 15:20:56 +0530

    add : nodemon and async handler in signup route

--- Server.js
+++ Server.js
@@ -44,7 +44,7 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema, "users");
 
 // Signup API endpoint
-app.post("/api/signup", (req, res) => {
+app.post("/api/signup", async (req, res) => {
   // Retrieve signup data from the request body
   const {
     username,
@@ -57,14 +57,12 @@ app.post("/api/signup", (req, res) => {
   } = req.body;
   console.log(username);
   // Check if the user already exists in the database
-  User.findOne({ email: email }, (error, existingUser) => {
-    if (error) {
-      console.error("Error finding user:", error);
-      res.status(500).json({ success: false, message: "Error checking user" });
-    } else if (existingUser) {
-      res.json({ success: false, message: "User already exists" });
-    } else {
-      // Create a new user instance
+  const user = await User.findOne({ email: email })
+      if(user){
+        res.status(404).send("user already exists")
+        return;
+      }
+
       const newUser = new User({
         username,
         password,
@@ -75,21 +73,13 @@ app.post("/api/signup", (req, res) => {
         gender,
       });
 
-      // Save the user to the database
-      newUser
-        .save()
-        .then(() => {
-          res.json({ success: true, message: "Signup successful" });
-        })
-        .catch((error) => {
-          console.error("Error saving user:", error);
-          res
-            .status(500)
-            .json({ success: false, message: "Error saving user" });
-        });
-    }
-  });
-});
+      await newUser.save();
+
+      res.status(200).json({"data" :"signup successfull"})
+
+
+     
+    });
 
 app.post("/api/login", async(req, res) => {
     // Retrieve login data from the request body
