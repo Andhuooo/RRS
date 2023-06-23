d1d798d75fdbcf564e3138d97622bfeccd1b0bb5
Anandhu P Asokkumar <anandhupasokkumar@Anandhus-MacBook-Air.local>
2023-06-23 13:12:25 +0530

    Add /Users/anandhupasokkumar/Desktop/rrs-app/src/Components/Userprofile.js

---
+++ src/Components/Userprofile.js
@@ -0,0 +1,28 @@
+
+import React, { useState } from "react";
+import "./Userprofile.css";
+import userprofile from "../userprofile.png";
+
+function Userprofile() {
+  const [user] = useState(localStorage.getItem("user"));
+
+  return (
+    <div className="user-profile-container">
+      <div className="user-profile-header">
+        <h2>User Profile</h2>
+      </div>
+      <div className="user-profile-content">
+        <div className="user-profile-picture">
+          <img src={userprofile} alt="User Profile" />
+        </div>
+        <div className="user-profile-details">
+          <h3>{user}</h3>
+          <p>Email: {user}</p>
+          {/* Add additional user details here */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Userprofile;
